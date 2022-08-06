a905fded14cc91cfc51386b68f0682ea59e3e24f
Pini Cheyni <pinich@gmail.com>
2022-08-06 15:55:09 +0300

    - BE configuration mostly done

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.env.PWD, process.env.CLIENT_PATH),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
